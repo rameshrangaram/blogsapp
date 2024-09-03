2ed37d6d1fc8bba5ad6f71adc6bac611a920ab00
Ramesh Rangaram <ramesh.rangaram@gmail.com>
2024-09-03 13:38:25 -0400

    Initial Commit

---
+++ src/main/java/org/rams/blogsapp/BlogsApplication.java
@@ -0,0 +1,11 @@
+package org.rams.blogsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogsApplication.class, args);
+    }
+}
---
+++ src/test/java/org/rams/blogsapp/BlogsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rams.blogsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BlogsApplicationTests {
+
+    @Test
+    void contextLoads(){
+
+    }
+}
